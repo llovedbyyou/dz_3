e71197b456db1e71644264ed48e23cb41f8ccf4e
llovedbyyou <varya.balya@yandex.ru>
2024-03-10 11:11:04 +0300

    Completed

---
+++ src/WeatherCalendar.java
@@ -0,0 +1,16 @@
+class WeatherCalendar {
+    String month;
+    int numberOfDays;
+    int rainyDays;
+    int year = 2020;
+    boolean isRainyMonth;
+
+    // Увеличить переменную rainyDays на единицу
+    public void addRainyDay() {
+        rainyDays += 1;
+    }
+    WeatherCalendar(String monthName, int monthNumberOfDays) {
+        monthName = month;
+        monthNumberOfDays = numberOfDays;
+    }
+}
